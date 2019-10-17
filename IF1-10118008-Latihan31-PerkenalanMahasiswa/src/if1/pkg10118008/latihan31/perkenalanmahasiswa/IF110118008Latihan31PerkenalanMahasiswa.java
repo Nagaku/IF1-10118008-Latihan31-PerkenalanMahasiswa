4d702f078626078bbeaa0e22eae91af130944622
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-10-17 19:19:15 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan31-PerkenalanMahasiswa/src/if1/pkg10118008/latihan31/perkenalanmahasiswa/IF110118008Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * * @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF-1
+ * NIM			: 10118008
+ * Deskripsi Program	: Menampilkan Perkenalan Mahasiswa
+ */
+public class IF110118008Latihan31PerkenalanMahasiswa {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		Mahasiswa mhs = new Mahasiswa();
+		Mahasiswa mhs1 = new Mahasiswa();
+		Mahasiswa mhs2 = new Mahasiswa();
+		Mahasiswa mhs3 = new Mahasiswa();
+		
+		
+		
+		mhs.perkenalkanDiri("Rizki Adama Kurniawan","1010269");
+		mhs1.perkenalkanDiri("Indra Tiola", "10110270");
+		mhs2.perkenalkanDiri("Robi Tanzil Ganefi", "10110271");
+		mhs3.perkenalkanDiri("Muhammad Nur Awaludin", "10110269");
+		
+		
+		
+		
+	}
+	
+}
